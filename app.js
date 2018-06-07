dc34729a733f9acaea8d195e56c86bed52fe592b
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-07 08:52:47 -0400

    Definición objeto literal

---
+++ app.js
@@ -0,0 +1,19 @@
+// objeto literal
+
+const dog = {
+  //key: value
+  raza: 'quiltro',
+  peso; 15,
+  nombre: 'Suave lomito',
+  color: 'negro'
+}
+
+
+// objeto constructor
+
+class Person  {     // -> La primera letra es mayúscula, así el programa reconoce que es un objeto constructor.
+  constructor (name, lastName) {
+    this.name = name;
+    this.lastName = lastName;
+  }
+}
